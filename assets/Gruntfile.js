30d4507515655049e5cb8c62bd6285b1d070dff1
Roman Klos <roman.klos@coex.cz>
2015-12-11 13:19:14 +0100

    Fix Typo for dist sass compilation

--- assets/Gruntfile.js
+++ assets/Gruntfile.js
@@ -23,7 +23,7 @@ module.exports = function(grunt) {
                 ]
             },
             dist: {
-                option: {
+                options: {
                     outputStyle: 'compressed'
                 },
                 files: {
