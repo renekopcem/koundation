a43cdb55764dc569ec4cc13689fb132aec0f3ee5
Roman Klos <roman.klos@coex.cz>
2015-12-11 13:18:48 +0100

    Remove load grunt tasks

--- assets/Gruntfile.js
+++ assets/Gruntfile.js
@@ -4,7 +4,6 @@ module.exports = function(grunt) {
     require('time-grunt')(grunt);
 
     // Load all tasks
-    //require('load-grunt-tasks')(grunt);
     require('jit-grunt')(grunt, {
         useminPrepare: 'grunt-usemin'
     });
